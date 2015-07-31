f89681d7916230e4d9704b67fd5a1ded6be2cba6
Blair Reeves <gaslight@gmail.com>
2015-07-30 22:08:00 -0400

    live

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '6ZPH58e8cSpbSoYGQIaFf1xEi'
-  , consumer_secret: 'dI3j9gEGgKonyt23oPAXdAucenpbQTWpARoqqtjTEb2eWCf4DA'
-  , access_token: '3298363897-gSAulwhf8yF7n5J1byJKYcCNujvuvHhe1qe6BTm'
-  , access_token_secret: '2vaMQJNbNmQEPn8Qgum8d5zrYtyqvsi9XI2HVWgSXaFbJ'
-}
