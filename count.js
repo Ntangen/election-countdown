4ec642c9689f96296b6910e4bb1d61e0343fa10b
Blair Reeves <gaslight@gmail.com>
2015-07-31 15:13:40 -0400

    update7.31.1

--- count.js
+++ count.js
@@ -1,35 +1,38 @@
 // the very latest in election detection
 
-var USElectionDay = new Date ("Nov 8, 2016, 19:00 GMT-0500");
-var now = new Date();
-var remaining = USElectionDay - now;
-var seconds = 1000;
-var minutes = seconds * 60;
-var hours = minutes * 60;
-var days = hours * 24;
-
-function SecsLeft () {
-	return Math.floor((remaining % minutes)/seconds);
-}
-
-function MinsLeft () {
-	return Math.floor((remaining % hours) / minutes);
-}
-
-function  HoursLeft () {
-	return Math.floor((remaining % days) / hours);
-}
-
-function DaysLeft () {
-	return Math.floor(remaining / days).toString();
-}
 exports.TimeLeft = function() {
+	var USElectionDay = new Date ("Nov 8, 2016, 19:00 GMT-0500");
+	var now = new Date();
+	var remaining = USElectionDay - now;
+	var seconds = 1000;
+	var minutes = seconds * 60;
+	var hours = minutes * 60;
+	var days = hours * 24;
+	
 	console.log("count function checkin");
+	
 	var blurb = " There are " + 
 		DaysLeft() + " days, " + 
-		// HoursLeft() + " hours, " + 
-		// MinsLeft() + " minutes, " +
-		// SecsLeft() + " seconds until the 2016 U.S. general election is over. " +
+		HoursLeft() + " hours, " + 
+		MinsLeft() + " minutes, " +
+		SecsLeft() + " seconds until the 2016 U.S. general election is over. " +
 		"Don't forget to vote! 🇺🇸" ;
 	return blurb;
+
+	function SecsLeft () {
+		return Math.floor((remaining % minutes)/seconds);
+	}
+
+	function MinsLeft () {
+		return Math.floor((remaining % hours) / minutes).toString();
+	}
+
+	function  HoursLeft () {
+		return Math.floor((remaining % days) / hours).toString();
+	}
+
+	function DaysLeft () {
+		return Math.floor(remaining / days).toString();
+	}
+
 }
--- mark1-stream.js
+++ mark1-stream.js
@@ -1,7 +1,7 @@
 // election countdown bot Mark 1 - streaming
 
 var config = require('./config');
-// var count = require('./count');
+var count = require('./count');
 var Twit = require('twit'); 
 var T = new Twit(config);
 
@@ -23,6 +23,14 @@ stream.on('tweet', function (tweet){
 	console.log("Got a tweet");
 	console.log("User: " + tweet.user.screen_name);
 	console.log("Text: " + tweet.text);
+	console.log('id: ' + tweet.id)
+	var thisTweet = count.TimeLeft();
+	T.post('statuses/update', {in_reply_to_status_id: tweet.id_str, status: "@" + 
+		tweet.user.screen_name + 
+		thisTweet}, function (err, data, res){
+			if (err) return handleError(err);
+			else console.log('tweet deployed!');
+			});
 });	
 
 
